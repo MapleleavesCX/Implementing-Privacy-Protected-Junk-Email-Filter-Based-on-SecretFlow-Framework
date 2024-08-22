7a30be819b8d34df4f8c8582268c5a270296ac87
MapleleavesCX <129497023+MapleleavesCX@users.noreply.github.com>
2024-08-23 00:50:36 +0800

    Add files via upload

---
+++ model_chooser/model/new_model_example.py
@@ -0,0 +1,28 @@
+
+
+# train训练额外参数参考列表：
+'''
+params = {
+
+}
+'''
+
+# 这里是一个新模型标准格式的示例：
+
+class SecureNewModel:
+
+    def __init__(self,  
+                 spu, 
+                 Server, Clients, 
+                 others):
+        None
+
+    def train(self, X_train, y_train, params):
+        None
+
+    def predict(self, X_test):
+        return None
+    
+# 示例使用
+if __name__ == '__main__':
+    None
